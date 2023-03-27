f4a62aafd643db01a65140b889e80b6951183d8a
LeonardoL3 <lelazzati7@gmail.com>
2023-03-26 21:27:07 -0300

    feat: add bishop movement logic

--- src/constants/CHESS_PIECES_MOVES.ts
+++ src/constants/CHESS_PIECES_MOVES.ts
@@ -1,6 +1,11 @@
 import { IBoard, IPieceInfo } from "@/hooks/useChessboard"
 import { mountMove } from "@/utils/mountMove"
 
+interface Indexes {
+  row: number
+  col: number
+}
+
 export function moves(pieceInfo: IPieceInfo, chessboard: IBoard[][], positionIndexes: {row: number, col: number}) {
   const axios = pieceInfo.color === 'white' ? '+' : '-'
   const isFirstMove = true
@@ -15,7 +20,7 @@ console.log('ai', positionIndexes)
   }
 
   function rook(){
-    const finalArray = [] as {row: number, col: number}[]
+    const finalArray = [] as Indexes[]
 
     for(let row = positionIndexes.row; row < chessboard.length; row++) {
       if(row+1 < chessboard.length) {
@@ -60,7 +65,90 @@ console.log('ai', positionIndexes)
     return finalArray
   }
 
-  function bishop(){}
+  function bishop(){
+    const finalArray = [] as Indexes[]
+
+    let initialColGoingUp = positionIndexes.col
+    for(let row = positionIndexes.row; row < chessboard.length; row++) {
+      if(row+1 <= 7 && initialColGoingUp+1 <= 7) {
+        if (!chessboard[row+1]?.[initialColGoingUp+1].pieceInfo) {
+          finalArray.push({row: row+1, col: initialColGoingUp+1})
+        } else {
+          break
+        }
+        initialColGoingUp++
+      }
+    }
+
+    let initialColWithRowGoingDown = positionIndexes.col
+    for(let row = positionIndexes.row; row >= 0; row--) {
+      if(row+1 <= 7 && initialColWithRowGoingDown+1 <= 7) {
+        if (!chessboard[row-1]?.[initialColWithRowGoingDown+1].pieceInfo) {
+          finalArray.push({row: row-1, col: initialColWithRowGoingDown+1})
+        } else {
+          break
+        }
+        initialColWithRowGoingDown++
+      }
+    }
+
+    let initialColGoingDown = positionIndexes.col
+    for(let row = positionIndexes.row; row < chessboard.length; row++) {
+      if(row+1 <= 7 && initialColGoingDown-1 >= 0) {
+        if (!chessboard[row+1]?.[initialColGoingDown-1].pieceInfo) {
+          finalArray.push({row: row+1, col: initialColGoingDown-1})
+        } else {
+          break
+        }
+        initialColGoingDown--
+      }
+    }
+
+    let initialCol = positionIndexes.col
+    for(let row = positionIndexes.row; row < chessboard.length; row--) {
+      if(row-1 >= 0 && initialCol-1 >= 0) {
+        if (!chessboard[row-1]?.[initialCol-1].pieceInfo) {
+          finalArray.push({row: row-1, col: initialCol-1})
+        } else {
+          break
+        }
+        initialCol--
+      }
+    }
+
+    console.log('eu sinto', finalArray)
+    return finalArray
+
+    //  for(let row = positionIndexes.row; row >= 0; row--) {
+    //   if(row-1 >= 0) {
+    //     if (!chessboard[row-1]?.[positionIndexes.col].pieceInfo) {
+    //       finalArray.push({row: row-1, col: positionIndexes.col})
+    //     } else {
+    //       break
+    //     }
+    //   }
+    // }
+
+    // for (let col = positionIndexes.col; col < chessboard[positionIndexes.row].length; col++) {
+    //   if (col+1 < chessboard[positionIndexes.row].length) {
+    //     if (!chessboard[positionIndexes.row][col+1]?.pieceInfo) {
+    //       finalArray.push({row: positionIndexes.row, col: col+1})
+    //     } else {
+    //       break
+    //     }
+    //   }
+    // }
+
+    //  for (let col = positionIndexes.col; col >= 0; col--) {
+    //   if (col-1 >= 0) {
+    //     if (!chessboard[positionIndexes.row][col+-1]?.pieceInfo) {
+    //       finalArray.push({row: positionIndexes.row, col: col-1})
+    //     } else {
+    //       break
+    //     }
+    //   }
+    // }
+  }
 
   function knight(){}
 
